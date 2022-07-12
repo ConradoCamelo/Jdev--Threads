8e792259dca1ac7d91db437690ab79d602914cb4
Conrado Camelo <Conrado Camelo@ConradoCamelo>
2022-07-12 15:46:19 -0300

    CM - Criando uma Thread Sleep tempo para imprimir no laço For.

--- Jdev - Thread/src/AulaThread/ClasseThread.java
+++ Jdev - Thread/src/AulaThread/ClasseThread.java
@@ -2,9 +2,12 @@ package AulaThread;
 
 public class ClasseThread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		for (int i = 0; i < 5; i++) {
+			
+			Thread.sleep(1000);//Isso vai fazer com que o programa demore um 1 segundo para imprimir de um laço para o outro. 
+			
 			System.out.println("Executando alguma rotina para trabalho e Envio de E-mail ");
 		}
 		
