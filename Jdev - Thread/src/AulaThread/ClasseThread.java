83829278716976d65556faf425ec242939c6a4ed
Conrado Camelo <Conrado Camelo@ConradoCamelo>
2022-07-12 15:39:53 -0300

    Aula Threads - Curso Jdev

---
+++ Jdev - Thread/src/AulaThread/ClasseThread.java
@@ -0,0 +1,14 @@
+package AulaThread;
+
+public class ClasseThread {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Executando alguma rotina para trabalho e Envio de E-mail ");
+		}
+		
+
+	}
+
+}
