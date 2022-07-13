75c185c3e92d7fbbcc73e46f12bea60c6b2c8c8b
Conrado Camelo <Conrado Camelo@ConradoCamelo>
2022-07-12 23:52:07 -0300

    CM - Criando oque vai aparecer dentro da tela, gerenciando posições de nomes e caixas

--- Jdev - Thread/src/AulaThread/TelaTimeThread.java
+++ Jdev - Thread/src/AulaThread/TelaTimeThread.java
@@ -1,10 +1,25 @@
 package AulaThread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TelaTimeThread extends JDialog {
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout()); //para criação do painel dentro da caixinha vazia (Painel de Componentes)
+	
+	private JLabel descricaoHora = new JLabel("Time da Thread 1	");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time da Thread 2	");
+	private JTextField mostraTempo2 = new JTextField();
+	
 
 	public TelaTimeThread() {/* Executa oque estiver dentro do momento da Abertura ou Execução, Tudo que escrevermos daqui para 
 	baixo ele vai excutar nessa tela do método SetVisible */
@@ -15,7 +30,28 @@ public class TelaTimeThread extends JDialog {
 		setResizable(false);//Travar para Ninguém mudar o tamnho da tela 
 		//Primeira Parte Concluida
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints(); // Controlador de posicionamento de botões na tela 
 		
+		gridBagConstraints.gridx = 0;// quer dizer que você quer iniciar na primeira linha (Pensar em um plano cartesiano)
+		gridBagConstraints.gridy = 0;// quer dizer que vai iniciar na primeira coluna  
+		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));//Um bloco
+		jPanel.add (descricaoHora, gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo, gridBagConstraints);//Fim bloco
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));//segundo bloco
+		gridBagConstraints.gridy ++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200,25));
+		gridBagConstraints.gridy ++;
+		jPanel.add(mostraTempo2, gridBagConstraints);//fim segundo bloco
+		
+		
+		add(jPanel,BorderLayout.WEST);
 		/*Sempre Será o Ultimo Comando*/
 		setVisible(true); //Torna a Tela visível para o usuário. 
 	}
