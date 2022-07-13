d012ab1c828e4a4b1ee79e831fece966a096a16e
Conrado Camelo <Conrado Camelo@ConradoCamelo>
2022-07-12 22:48:03 -0300

    CM - Instanciando um Objeto para as Threads

--- Jdev - Thread/src/AulaThread/ClasseThread.java
+++ Jdev - Thread/src/AulaThread/ClasseThread.java
@@ -6,29 +6,53 @@ public class ClasseThread {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		new Thread() {
-			
-			public void run() {;//Aqui executa oque nós queremos
-			
-				for (int i = 0; i < 5; i++) {
-					
-					try {
-						Thread.sleep(1000);//Isso vai fazer com que o programa demore um 1 segundo para imprimir de um laço para o outro. 
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					
-					System.out.println("Executando alguma rotina para trabalho e Envio de E-mail ");
-				}
-			}
-			
-		}.start(); //Start é o que liga a Thread pra ficar processando paralelamente 
+		//Instanciando um objeto de forma diferente - Prestar atenção porque muda da forma padrão um pouco. 
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 		
-		
-		
-		System.out.println("Chegando ao Fim do código");
+		//Instanciando um objeto de forma diferente - Prestar atenção porque muda da forma padrão um pouco. 
+		Thread threadNotaFiscal = new Thread(thread2);
+		threadNotaFiscal.start();
+
+		System.out.println("Chegando ao Fim de fluxo antes de finalizar a Thread inteira, Observe!");
 		JOptionPane.showMessageDialog(null, "Código chegando ao fim");
 
 	}
 
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 5; i++) {
+
+				try {
+					Thread.sleep(1000);// Isso vai fazer com que o programa demore um 1 segundo para imprimir de um
+										// laço para o outro.
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				System.out.println("Executando alguma rotina para trabalho e Envio de E-mail ");
+			}
+
+		}
+	};
+	
+	private static Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			for (int i = 0; i < 5; i++) {
+
+				try {
+					Thread.sleep(2000);// Isso vai fazer com que o programa demore um 1 segundo para imprimir de um
+										// laço para o outro.
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				System.out.println("Executando alguma rotina para trabalho de NOta fiscal ");
+			}
+		}
+	};
 }
